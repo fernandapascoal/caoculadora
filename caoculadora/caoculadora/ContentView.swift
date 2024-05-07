299d3292412b389886a80f5206e1ed8e579d03e8
Fernanda Pascoal <pascfernanda@gmail.com>
2024-05-07 16:12:00 -0300

    adiciona arquivos do projeto

---
+++ caoculadora/caoculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  caoculadora
+//
+//  Created by Fernanda Pascoal on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "pawprint")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ caoculadora/caoculadora/caoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  caoculadoraApp.swift
+//  caoculadora
+//
+//  Created by Fernanda Pascoal on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct caoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
